0152c6c0859b8b6de8feface4a38c3050b1ce5d2
Alberto De Bortoli <albertodebortoli@gmail.com>
2013-08-01 23:22:33 +0100

    Initial Commit

---
+++ ADBObjectLocker/ADBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ADBAppDelegate.h
+//  ADBObjectLocker
+//
+//  Created by Alberto De Bortoli on 01/08/2013.
+//  Copyright (c) 2013 Alberto De Bortoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ADBObjectLocker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ADBObjectLocker
+//
+//  Created by Alberto De Bortoli on 01/08/2013.
+//  Copyright (c) 2013 Alberto De Bortoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ADBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ADBAppDelegate class]));
+    }
+}
